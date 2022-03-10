0a5d8c8055a9899c9b5cbadbe7d7f3fa69577fa5
Jatin-Goyal5 <jatinji145@gmail.com>
2022-03-10 18:46:29 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FacebookLoginProvider, GoogleLoginProvider, SocialAuthService } from 'angularx-social-login';
+
+import { HttpClient } from "@angular/common/http";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'google-auth';
+  constructor(private router: Router,
+    private socialAuthService: SocialAuthService,
+    private http: HttpClient) {
+}
+
+loginWithGoogle(): void {
+this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)
+.then((resp) => {
+  console.log(resp);
+  // this.http.post<{status:{},data:any}>("http://localhost:3071/usersocial/token",{token:resp.response.id_token})
+  //       .subscribe((resp)=>{
+  //       },error=>{
+  //         console.log("login failed",error);
+  //       });
+  this.router.navigate(['/'])
+});
+
+
+}
+
+signInWithFB(): void {
+  this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID).then((resp)=>{
+    console.log("facebook->",resp)
+  });
+}
+
+signInwithTrueCaller(): void {
+  window.location.href = `truecallersdk://truesdk/web_verify?requestNonce=8585937950
+                          &partnerKey=edIsh8e9964cad1314f80aff7a718787fe7e0
+                          &partnerName=none
+                          &lang=en
+                          &title=TITLE_STRING_OPTION`;
+
+    setTimeout(()=>{
+
+      if( document.hasFocus() ){
+        alert('not present');
+      }
+      else{
+        alert('check message');
+        // Truecaller app present on the device and the profile overlay opens
+        // The user clicks on verify & you'll receive the user's access token to fetch the profile on your
+        // callback URL - post which, you can refresh the session at your frontend and complete the user  verification
+      }
+    }, 600);
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FacebookLoginProvider, GoogleLoginProvider, SocialLoginModule} from 'angularx-social-login';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+
+const googleLoginOptions = {
+  scope: 'profile email',
+  redirect_uri:'https://dev.apis.mogiio.com/usersocial/token'
+};
+
 
 @NgModule({
   declarations: [
@@ -10,9 +19,30 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+ ReactiveFormsModule,
+ SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: true, //keeps the user signed in
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('353919017162-433me9icip917i3f8m2s5ram8ivt5vhu.apps.googleusercontent.com', googleLoginOptions) // your client id
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('286164873654194')
+          }
+        ]
+      }
+    },
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
